ef6082fa85574805c2d42932df0a36097d66f4d0
24prathamesh2004 <24prathamesh2004@gmail.com>
2026-06-18 23:57:13 +0530

    added nitro

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,13 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import { nitro } from "nitro/vite";
 import path from "path";
 
 export default defineConfig({
   plugins: [
     tanstackStart(),
+    nitro(),
     react(),
     tailwindcss(),
   ],
